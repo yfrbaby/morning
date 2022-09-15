dd834033916808dd28b1df0584440a1913e4c6db
yfrbaby <112079901+yfrbaby@users.noreply.github.com>
2022-09-15 12:13:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import json
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -43,11 +44,17 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
+def get_xingzuo():
+  xingzuo = requests.get("http://web.juhe.cn:8080/constellation/getAll?consName=%E6%91%A9%E7%BE%AF%E5%BA%A7&type=today&key=5d4c067629b359a72ee6c0c2008ccf3d")
+  if xingzuo.status_code !=200:
+    return get_xingzuo()
+  return json.loads(xingzuo.text)
+
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()},'QFriend':get_xingzuo()[QFriend]}
 res = wm.send_template(user_id, template_id, data)
 print(res)
