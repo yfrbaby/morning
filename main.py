9a645a560001926bc26c147dc4d63cc45640de2b
yfrbaby <112079901+yfrbaby@users.noreply.github.com>
2022-12-10 23:46:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,12 @@ def get_xingzuo():
 def get_xingzuo1(x):
   xingzuo = requests.get("http://web.juhe.cn/constellation/getAll?consName=%E5%A4%84%E5%A5%B3%E5%BA%A7&type=today&key=5d4c067629b359a72ee6c0c2008ccf3d")
   return xingzuo.json()[x]
+def get_xingzuo2():
+  xingzuo = requests.get("http://web.juhe.cn/constellation/getAll?consName=%E6%91%A9%E7%BE%AF%E5%BA%A7&type=today&key=5d4c067629b359a72ee6c0c2008ccf3d")
+  return xingzuo.json()['summary']
+def get_xingzuo3(x):
+  xingzuo = requests.get("http://web.juhe.cn/constellation/getAll?consName=%E6%91%A9%E7%BE%AF%E5%BA%A7&type=today&key=5d4c067629b359a72ee6c0c2008ccf3d")
+  return xingzuo.json()[x]
 
 client = WeChatClient(app_id, app_secret)
 
@@ -58,3 +64,5 @@ wea, temperature = get_weather()
 data = {'QFriend':{'value':get_xingzuo1('QFriend')},'number':{'value':get_xingzuo1('number')},'money':{'value':get_xingzuo1('money')},'work':{'value':get_xingzuo1('work')},'love':{'value':get_xingzuo1('love')},'health':{'value':get_xingzuo1('health')},'luckycolor':{'value':get_xingzuo1('color')},"summary":{'value':get_xingzuo(), "color":get_random_color()},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
+data2 = {'QFriend':{'value':get_xingzuo3('QFriend')},'number':{'value':get_xingzuo3('number')},'money':{'value':get_xingzuo3('money')},'work':{'value':get_xingzuo3('work')},'love':{'value':get_xingzuo3('love')},'health':{'value':get_xingzuo3('health')},'luckycolor':{'value':get_xingzuo3('color')},"summary":{'value':get_xingzuo2(), "color":get_random_color()},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+res2 = wm.send_template(user_id2, template_id2, data2)
